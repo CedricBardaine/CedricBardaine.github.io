3ba5742f4a03c7269f1e49955d1471fe71120acb
Cédric B <cedricbardaine@gmail.com>
2020-10-20 12:34:27 +0200

    indent & clean

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,17 @@
 <template>
   <v-app>
     <v-row no-gutters>
+
       <MenuDrawer v-if="!isSoSmall" />
       <MenuFooter v-else style="position: fixed ; height: 48px ; z-index: 99"/>
-      <!-- <v-app-bar></v-app-bar> -->
+
     <v-col :style="isSoSmall ? 'padding-top: 48px' : ''">
       <v-main>
         <router-view/>
       </v-main>
     </v-col>
   </v-row>
+  
   </v-app>
 </template>
 
--- src/components/MenuFooter.vue
+++ src/components/MenuFooter.vue
@@ -1,11 +1,9 @@
 <template>
-  
   <v-footer
   color="primary"
   dark
-    width="100%"
-    style="place-content: center;"
-
+  width="100%"
+  style="place-content: center;"
   >
     <v-card
     flat
@@ -23,34 +21,12 @@
       class="mx-1"
       icon
       >
-      <v-icon size="24px">
-        {{ item.icon }}
-      </v-icon>
-    </v-btn>
-  </v-card>
-
-<!-- style="width: unset;" to make the width at the minimmum (default width is set by Vuetify)-->
-<!-- <v-list dense nav>
-  <v-list-item
-  v-for="item in items"
-  :key="item.title"
-  link
-  @click="
-  $router.history.current.name != item.goto ?
-  $router.push({name: item.goto}) : 
-  ''
-  "
-  >
-  <v-list-item-icon>
-    <v-icon >{{ item.icon }}</v-icon>
-  </v-list-item-icon>
-  
-  <v-list-item-content>
-    <v-list-item-title >{{ item.title }}</v-list-item-title>
-  </v-list-item-content>
-</v-list-item>
-</v-list> -->
-</v-footer>
+        <v-icon size="24px">
+          {{ item.icon }}
+        </v-icon>
+      </v-btn>
+    </v-card>
+  </v-footer>
 </template>
 
 <script>
@@ -62,28 +38,33 @@
         // icon: 'mdi-cat',
         // goto: 'Cat'
         // },
-        {title: '',
-        icon: 'mdi-home',
-        goto: 'Home',
-      },
-      {title: 'Contactez moi',
-      icon: 'mdi-account-box',
-      goto: 'Contact',
+        {
+          title: '',
+          icon: 'mdi-home',
+          goto: 'Home',
+        },
+        {
+          title: 'Contactez moi',
+          icon: 'mdi-account-box',
+          goto: 'Contact',
+        },
+        {
+          title: 'Diplômes',
+          icon: 'mdi-certificate',
+          goto: 'Diplomas',
+        },
+        {
+          title: 'Compétences',
+          icon: 'mdi-head-snowflake ',
+          goto: 'SkillsList',
+        },
+        {
+          title: 'Mon GitHub',
+          icon: 'mdi-github',
+          goto: 'MyGitHub',
+        },
+        ],
+      };
     },
-    {title: 'Diplômes',
-    icon: 'mdi-certificate',
-    goto: 'Diplomas',
-  },
-  {title: 'Compétences',
-  icon: 'mdi-head-snowflake ',
-  goto: 'SkillsList',
-},
-{title: 'Mon GitHub',
-icon: 'mdi-github',
-goto: 'MyGitHub',
-},
-],
-};
-},
-};
+  };
 </script>
