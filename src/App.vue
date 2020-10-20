62e0d710b3886bfba79f9f380a2ebed4a9fb1a40
Cédric B <cedricbardaine@gmail.com>
2020-10-20 12:29:54 +0200

    vIf the NavMenuDrawer into a NavFooter on very xs screens

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,10 @@
 <template>
   <v-app>
     <v-row no-gutters>
-
-
-      <MenuDrawer />
+      <MenuDrawer v-if="!isSoSmall" />
+      <MenuFooter v-else style="position: fixed ; height: 48px ; z-index: 99"/>
       <!-- <v-app-bar></v-app-bar> -->
-    <v-col>
+    <v-col :style="isSoSmall ? 'padding-top: 48px' : ''">
       <v-main>
         <router-view/>
       </v-main>
@@ -16,10 +15,15 @@
 
 <script>
 import MenuDrawer from '@/components/MenuDrawer.vue';
+import MenuFooter from '@/components/MenuFooter.vue';
 
 export default {
   components: {
     MenuDrawer,
+    MenuFooter,
+  },
+  computed: {
+    isSoSmall() {return this.$vuetify.breakpoint.width < 400 }
   },
 };
 </script>
---
+++ src/components/MenuFooter.vue
@@ -0,0 +1,89 @@
+<template>
+  
+  <v-footer
+  color="primary"
+  dark
+    width="100%"
+    style="place-content: center;"
+
+  >
+    <v-card
+    flat
+    tile
+    class="primary text-center"
+    >
+      <v-btn
+      v-for="item in items"
+      :key="item.title"
+      @click="
+      $router.history.current.name != item.goto ?
+      $router.push({name: item.goto}) : 
+      ''
+      "
+      class="mx-1"
+      icon
+      >
+      <v-icon size="24px">
+        {{ item.icon }}
+      </v-icon>
+    </v-btn>
+  </v-card>
+
+<!-- style="width: unset;" to make the width at the minimmum (default width is set by Vuetify)-->
+<!-- <v-list dense nav>
+  <v-list-item
+  v-for="item in items"
+  :key="item.title"
+  link
+  @click="
+  $router.history.current.name != item.goto ?
+  $router.push({name: item.goto}) : 
+  ''
+  "
+  >
+  <v-list-item-icon>
+    <v-icon >{{ item.icon }}</v-icon>
+  </v-list-item-icon>
+  
+  <v-list-item-content>
+    <v-list-item-title >{{ item.title }}</v-list-item-title>
+  </v-list-item-content>
+</v-list-item>
+</v-list> -->
+</v-footer>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        items: [
+        // { title: '...',
+        // icon: 'mdi-cat',
+        // goto: 'Cat'
+        // },
+        {title: '',
+        icon: 'mdi-home',
+        goto: 'Home',
+      },
+      {title: 'Contactez moi',
+      icon: 'mdi-account-box',
+      goto: 'Contact',
+    },
+    {title: 'Diplômes',
+    icon: 'mdi-certificate',
+    goto: 'Diplomas',
+  },
+  {title: 'Compétences',
+  icon: 'mdi-head-snowflake ',
+  goto: 'SkillsList',
+},
+{title: 'Mon GitHub',
+icon: 'mdi-github',
+goto: 'MyGitHub',
+},
+],
+};
+},
+};
+</script>
